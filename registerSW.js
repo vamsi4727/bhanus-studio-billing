b080138dfdff530abf62b0c39c8548804c56ce78
Vamsikrishna.k <katakam.vamsi@gmail.com>
2026-01-11 17:30:43 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/bhanus-studio-billing/sw.js', { scope: '/bhanus-studio-billing/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
